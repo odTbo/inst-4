b033d6c93b5cacb4c541270bf16b47f730cb295c
Vladimir <vladimirpolak2@gmail.com>
2021-09-19 20:17:07 +0200

    Inst-4

--- modules/instagram_manager.py
+++ modules/instagram_manager.py
@@ -222,6 +222,7 @@ class Instagram(Logs):
             posts.extend(results.get('items', []))
             next_max_id = results.get('next_max_id')
             while next_max_id:
+                print(len(posts))
                 timeout()
 
                 results = self.api.user_feed(user_id, max_id=next_max_id)
