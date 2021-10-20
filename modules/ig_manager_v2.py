faa0c25041a9b5299966e82aa1f11d8646518520
Vladimir <vladimirpolak2@gmail.com>
2021-10-21 01:45:01 +0200

    instagram manager V2 TODO fetch followers

--- modules/ig_manager_v2.py
+++ modules/ig_manager_v2.py
@@ -8,9 +8,10 @@ from os import path, getenv
 from instagrapi import Client
 
 try:
-    from modules.device_info import my_settings
+    from modules.device_info import UserSettings
 except ImportError:
-    my_settings = {}
+    UserSettings = None
+    pass
 
 load_dotenv()
 
@@ -36,48 +37,68 @@ class Instagram(LogsMixin):
         # load_settings(path: Path)	    dict	Load session settings from file
         # dump_settings(path: Path)	    bool	Serialize and save session settings to file
 
-        custom_settings = {
-            "cpu": my_settings["phone_chipset"],
-            "dpi": my_settings["phone_dpi"],
-            "model": my_settings["phone_model"],
-            "device": my_settings["phone_device"],
-            "resolution": my_settings["phone_resolution"],
-            "app_version": my_settings["app_version"],
-            "manufacturer": my_settings["phone_manufacturer"],
-            "version_code": my_settings["version_code"],
-            "android_release": my_settings["android_release"],
-            "android_version": my_settings["android_version"]
-        }
-        settings = {}
+        custom_settings = self.custom_settings()
 
         cached_settings = Path().cwd().parent / "cached_settings.json"
 
         # if saved settings, login with saved settings
         if cached_settings.exists():
+            print("Reusing cached settings.")
             self.api = Client()
             self.api.load_settings(cached_settings)
 
         # else new login and save settings
         else:
-            settings["device_settings"] = custom_settings
-            settings["user_agent"] = my_settings["user_agent"]
+            print("New login session.")
+            if custom_settings:
+                self.api = Client(custom_settings)
 
-            self.api = Client(settings)
             self.api.login(self.username, self.password)
 
             self.api.dump_settings(cached_settings)
 
+
+    def custom_settings(self):
+        """Creates custom device settings for client if provided."""
+        if UserSettings:
+            settings = {
+                "user_agent": UserSettings.USER_AGENT,
+                "device_settings": {
+                    "cpu": UserSettings.PHONE_CHIPSET,
+                    "dpi": UserSettings.PHONE_DPI,
+                    "model": UserSettings.PHONE_MODEL,
+                    "device": UserSettings.PHONE_DEVICE,
+                    "resolution": UserSettings.PHONE_RESOLUTION,
+                    "app_version": UserSettings.APP_VERSION,
+                    "manufacturer": UserSettings.PHONE_MANUFACTURER,
+                    "version_code": UserSettings.VERSION_CODE,
+                    "android_release": UserSettings.ANDROID_RELEASE,
+                    "android_version": UserSettings.ANDROID_VERSION
+                }
+            }
+            return settings
+        return None
+
+
     def fetch_following(self, target_account: str, all_=False) -> list:
         """Grabs target account's following."""
 
-
+    # LEGACY
     def get_user_id(self, username: str) -> int:
-        """Username to user_id"""
+        """
+        OBSOLETE
+        use 'api.user_id_from_username()'
+        """
+        # self.api.user_id_from_username()
+        pass
 
 
-    def fetch_followers(self, target_account: str, all_=False) -> list:
+    def fetch_followers(self, target_account: int, all_=False) -> list:
         """Grabs followers from target account."""
+        users = self.api.user_followers(user_id=target_account, amount=50)
 
+
+        return users
 
     def follow_user(self, user_id: int) -> None:
         """Follow IG User by username."""
@@ -117,6 +138,7 @@ class Instagram(LogsMixin):
 if __name__ == "__main__":
     ig = Instagram()
     ig.login()
-    acc_info = ig.api.account_info()
-    print(acc_info.username)
+
+    user_id = ig.api.user_id_from_username("soulhoe")
+    users = ig.fetch_followers(user_id)
 
